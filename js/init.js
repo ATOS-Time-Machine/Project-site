58e458db2385ec7581609c16b67dd628f01b20fa
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2016-12-12 02:15:16 +0000

    Overhaul.

--- js/init.js
+++ js/init.js
@@ -13,3 +13,12 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('.materialboxed').materialbox();
 });
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+});
+
+$(document).ready(function(){
+    $('ul.tabs').tabs();
+});
