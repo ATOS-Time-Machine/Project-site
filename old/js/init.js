499811cd259cfe693aca127b3b508a3be6ec1793
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2017-03-19 14:41:03 +0000

    Moved old site to old dir. Ready for new site

---
+++ old/js/init.js
@@ -0,0 +1,38 @@
+(function($){
+    $(function(){
+
+        $('.button-collapse').sideNav();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+$(document).ready(function(){
+    $('.slider').slider({full_width: true});
+});
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+});
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+});
+
+$(document).ready(function(){
+    $('ul.tabs').tabs();
+});
+
+$(".dropdown-button").dropdown();
+
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+});
+
+// $(document).ready(function(){
+//     $('.target').pushpin({
+//         // top: 0,
+//         bottom: 1000,
+//         offset: 0
+//     });
+// });
