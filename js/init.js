f3f62a33d55b79a1c24e9730a7264c72756d0b6e
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2017-03-21 11:55:39 +0000

    Added scrolling top bar... Bugs all over though :P

--- js/init.js
+++ js/init.js
@@ -5,3 +5,14 @@ $(document).ready(function () {
 $(document).ready(function () {
   $('.parallax').parallax();
 });
+
+$(document).ready(function () {
+  $('.pushpin').each(function() {
+    var $this = $(this);
+    var $target = $('#' + $(this).attr('data-target'));
+    $this.pushpin({
+      top: $target.offset().top,
+      bottom: $target.offset().top + $target.outerHeight() - $this.height()
+    });
+  });
+})
