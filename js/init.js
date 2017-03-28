9ef84aa8342422ce0f897499b87bd842fc30de76
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2017-03-28 15:31:02 +0100

    Table of contents working for api page

--- js/init.js
+++ js/init.js
@@ -4,15 +4,6 @@ $(document).ready(function () {
 
   $('.parallax').parallax();
 
-  $('.pushpin').each(function() {
-    var $this = $(this);
-    var $target = $('#' + $(this).attr('data-target'));
-    $this.pushpin({
-      top: $target.offset().top,
-      bottom: $target.offset().top + $target.outerHeight() - $this.height()
-    });
-  });
-
   $('.carousel.carousel-slider').carousel({fullWidth: true});
 
   $('.slider').slider();
